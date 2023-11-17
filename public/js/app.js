12123503bbe41ef388afc6c45493e5b0605691c5
Acuña Melanie <melaniexiomara485@gmail.com>
2023-11-17 16:37:37 -0300

    Agregue un select a materias innecesarias

--- public/js/app.js
+++ public/js/app.js
@@ -47,7 +47,7 @@ questionCareer.addEventListener('change',()=>{
             <option value="Ingles tecnico">Ingles tecnico</option>
             <option value="Comunicaciones analogicas">Comunicaciones analogicas</option>
             <option value="Laboratorio de electromagnetismo aplicado">Laboratorio de electromagnetismo aplicado</option>
-            <option value="Comunicaciones degitales">Comunicaciones degitales</option>
+            <option value="Comunicaciones digitales">Comunicaciones digitales</option>
             <option value="Laboratorio de redes de datos y comunicaciones">Laboratorio de redes de datos y comunicaciones</option>
             <option value="Gestion y mantenimiento de calidad">Gestion y mantenimiento de calidad</option>
             <option value="Sistemas opticos">Sistemas opticos</option>
@@ -102,6 +102,97 @@ questionCareer.addEventListener('change',()=>{
     }
 
 })
+
+questionCareer.addEventListener('change',()=>{
+    switch (questionCareer.value) {
+        case ("TEC. SUP. EN DESARROLLO DE SOFTWARE"):{
+            questionUnnecessary.innerHTML = ``
+            questionUnnecessary.innerHTML += `
+            <option value="Arquitectura y sistemas operativos"> Arquitectura y sistemas operativos</option>
+            <option value="Algoritmos y estructura de datos"> Algoritmos y estructura de datos</option>
+            <option value="Taller de lenguaje de programacion"> Taller de lenguaje de programacion</option>
+            <option value="Base de datos"> Base de datos</option>
+            <option value="Practica Profesional"> Practica Profesional</option>
+            <option value="Laboratorio de Ingles"> Laboratorio de Ingles</option>
+            <option value="Matematica aplicada"> Matematica aplicada</option>
+            <option value="Tecnologia y sociedad"> Tecnologia y sociedad</option>
+            <option value="Ingenieria de software"> Ingenieria de software</option>
+            <option value="Seminarios de talleres de actualizacion"> Seminarios de talleres de actualizacion</option>
+            <option value="Orientacion en valores y motivacional"> Orientacion en valores y motivacional</option>
+            <option value="Todas son necesarias">Todas son necesarias</option>
+            `
+            break;
+        }case ('TEC. SUP. TELECOMUNICACIONES' ):{
+            questionUnnecessary.innerHTML = ``
+            questionUnnecessary.innerHTML += `
+            <option value="Calculo aplicado a las telecomunicaciones">Calculo aplicado a las telecomunicaciones</option>
+            <option value="Fisica aplicada a las telecomunicaciones">Fisica aplicada a las telecomunicaciones</option>
+            <option value="Señales y sistemas continuos y discretos">Señales y sistemas continuos y discretos</option>
+            <option value="Introduccion a la progamacion">Introduccion a la progamacion</option>
+            <option value="Fundamentos de las telecomunicaciones">Fundamentos de las telecomunicaciones</option>
+            <option value="Laboratorio de electrotecnia, electronica analogica y digital">Laboratorio de electrotecnia, electronica analogica y digital</option>
+            <option value="Ingles tecnico">Ingles tecnico</option>
+            <option value="Comunicaciones analogicas">Comunicaciones analogicas</option>
+            <option value="Laboratorio de electromagnetismo aplicado">Laboratorio de electromagnetismo aplicado</option>
+            <option value="Comunicaciones digitales">Comunicaciones digitales</option>
+            <option value="Laboratorio de redes de datos y comunicaciones">Laboratorio de redes de datos y comunicaciones</option>
+            <option value="Gestion y mantenimiento de calidad">Gestion y mantenimiento de calidad</option>
+            <option value="Sistemas opticos">Sistemas opticos</option>
+            <option value="Normativas de comunicaciones">Normativas de comunicaciones</option>
+            <option value="Economia y proyectos">Economia y proyectos</option>
+            <option value="Seguridad e higiene industrial">Seguridad e higiene industrial</option>
+            <option value="Todas son necesarias">Todas son necesarias</option>
+            `
+            break;
+        }
+
+        case ('TEC. SUP. MECATRÓNICA'):{
+            questionUnnecessary.innerHTML = ``
+            questionUnnecessary.innerHTML += `
+            <option value="Ingles tecnico">Ingles tecnico</option>
+            <option value="Seguridad e Higiene industrial">Seguridad e Higiene industrial</option>
+            <option value="Metro fisica">Metro fisica</option>
+            <option value="Elementos de maquinas">Elementos de maquinas</option>
+            <option value="Taller de dibujo tecnico">Taller de dibujo tecnico</option>
+            <option value="Taller de matematica">Taller de matematica</option>
+            <option value="Sistema hidraulicos y neumaticos">Sistema hidraulicos y neumaticos</option>
+            <option value="Sistema electricos y electronicos">Sistema electricos y electronicos</option>
+            <option value="Introduccion a la mecatronica">Introduccion a la mecatronica</option>
+            <option value="Taller de electricidad y electronica">Taller de electricidad y electronica</option>
+            <option value="Diseño industrial y CAD">Diseño industrial y CAD</option>
+            <option value="Sistema mecanico">Sistema mecanico</option>
+            <option value="Integracion de sistemas">Integracion de sistemas</option>
+            <option value="Robotica">Robotica</option>
+            <option value="Todas son necesarias">Todas son necesarias</option>
+            `
+            break;
+        }case('TEC. SUP. EN QUIMICA INDUSTRIAL'):{
+            questionUnnecessary.innerHTML = ``
+            questionUnnecessary.innerHTML += `
+            <option value="Analisis de dato">Analisis de datos</option>
+            <option value="Formulacion y preparacion de mezcla">Formulacion y preparacion de mezclas</option>
+            <option value="Operaciones basicas en la industria quimic">Operaciones basicas en la industria quimica</option>
+            <option value="Generacion y recuperacion de energi">Generacion y recuperacion de energia</option>
+            <option value="Practica profesionalizant">Practica profesionalizante</option>
+            <option value="Ingles tecnic">Ingles tecnico</option>
+            <option value="Acondicionamiento y almacenamiento de productos quimico">Acondicionamiento y almacenamiento de productos quimicos</option>
+            <option value="Regulacion y control de procesos quimico">Regulacion y control de procesos quimicos</option>
+            <option value="Mantenimiento electromecanico en insdustrias de proces">Mantenimiento electromecanico en insdustrias de proceso</option>
+            <option value="prevencion de riesgos en industrias quimica">prevencion de riesgos en industrias quimicas</option>
+            <option value="Formacion y orientacion labora">Formacion y orientacion laboral</option>
+            <option value="Transporte de solidos y fluido">Transporte de solidos y fluidos</option>
+            <option value="Reactores quimico">Reactores quimicos</option>
+            <option value="Comunicacio">Comunicacion</option>
+            <option value="Ciencia sociedad y estad">Ciencia sociedad y estado</option>
+            <option value="Gestion de emprendimientos">Gestion de emprendimientos</option>
+            <option value="Todas son necesarias">Todas son necesarias</option>
+            `
+            break;
+        }
+    }
+
+})
+
 formEncuesta.addEventListener('submit',async(e)=>{
     e.preventDefault();
     const formData = new FormData();
